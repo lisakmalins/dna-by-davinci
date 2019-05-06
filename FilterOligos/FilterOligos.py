867536de3991056bafcb1ec30d84331ec45374e6
Lisa Malins <lkmalins@ucdavis.edu>
2019-05-06 12:24:42 -0700

    Fixed bugs in bwa filter, finished primer3 filter

--- FilterOligos/FilterOligos.py
+++ FilterOligos/FilterOligos.py
@@ -28,7 +28,7 @@ def isheader(line):
         return False
 
 # Filter out sequences with less than 70% homology
-def bwafilter(line, min_AS, max_XS):
+def bwa_filter(line, min_AS, max_XS):
     """
     :param line: line of sam file
     :param min_AS: min AS:i score, suggest probe length
@@ -39,8 +39,8 @@ def bwafilter(line, min_AS, max_XS):
     # Generate patterns that will be used to search for
     # alignment scores (AS) and suboptimal alignment scores (XS)
     # TODO can we not compile this every time?
-    AS_pat = re.compile('AS:i:(\d.)')
-    XS_pat = re.compile('XS:i:(\d.)')
+    AS_pat = re.compile('AS:i:(\d*)')
+    XS_pat = re.compile('XS:i:(\d*)')
 
     # Search for an alignment score and a suboptimal alignment score
     # If found, save as match object (see re documentation)
@@ -51,14 +51,21 @@ def bwafilter(line, min_AS, max_XS):
     # If an alignment score and a suboptimal alignment score
     # are found, grab the scores and save for comparison to minimum and maximum.
     # If either score isn't found, return true (ignore this line)
+
+
+    mapinfo = line.split('\t') #debug
+
+
     if AS_match:
         AS_score = int(AS_match.group(1))
     else:
+        print("AS score not found for sequence " + mapinfo[0] + " " + mapinfo[14].rstrip('\n')) #debug
         return True
 
     if XS_match:
         XS_score = int(XS_match.group(1))
     else:
+        print("XS score not found for sequence " + mapinfo[0] + " " + mapinfo[14].rstrip('\n')) #debug
         return True
 
     # print("AS = " + str(AS_score) + " min_AS = " + str(min_AS) + " XS = " + str(XS_score) + " max_XS =" + str(max_XS))
@@ -67,11 +74,14 @@ def bwafilter(line, min_AS, max_XS):
     # If the alignment score is less than the minimum
     # or the suboptimal alignment score is greater than the maximum,
     # return true (filter out this sequence because we don't like it)
-    if (AS_score <= min_AS) & (XS_score > max_XS):
+    if (AS_score < min_AS) or (XS_score >= max_XS):
 
-        # mapinfo = line.split('\t')
-        #
-        # seqlist.append(mapinfo[9])
+        mapinfo = line.split('\t')
+
+        if (AS_score < min_AS):
+            print(mapinfo[0] + " filtered because AS score " + str(AS_score) + " < " + str(min_AS))
+        if (XS_score >= max_XS):
+            print(mapinfo[0] + " filtered because XS score " + str(XS_score) + " >= " + str(max_XS))
 
         # print("AS score of " + str(AS_score) + " is greater than or equal to " + str(min_AS))
         # print("XS score of " + str(XS_score) + " is less than" + str(max_XS))
@@ -83,10 +93,8 @@ def bwafilter(line, min_AS, max_XS):
     else:
         return False
 
-
-# Screens derpy oligos by making sure that: melting temperature is above the minimum,
-# hairpin melting temperature is below the maximum, and the difference between the two is sufficient.
-def primer3_filter(line, min_TM=37, max_TM=35, min_diff_TM=10):
+# Filter out derpy oligos
+def primer3_filter(line, min_TM=37, max_HTM=35, min_diff_TM=10):
     """
     :param sequence: sequence of oligo
     :param min_TM: minimum melting temp, default 37
@@ -109,7 +117,7 @@ def primer3_filter(line, min_TM=37, max_TM=35, min_diff_TM=10):
         primer3ft = True
 
     # If hairpin melting temperature is too high, filter out
-    if HTM > max_TM:
+    if HTM > max_HTM:
         primer3ft = True
 
     # If melting temperature and hairpin melting temperature
@@ -121,11 +129,8 @@ def primer3_filter(line, min_TM=37, max_TM=35, min_diff_TM=10):
 
     return primer3ft
 
-
-#-------------------main-----------------------
-
-if __name__ == '__main__':
-
+# Set up source and output files (returns tuple of file objects)
+def setupio():
     # Read arguments from command line
     try:
         source_name = sys.argv[1]
@@ -134,18 +139,29 @@ if __name__ == '__main__':
 
     # Use hard-codes file names if arguments not given
     except IndexError:
-        source_name = "tinymap.sam"
-        output_name = "/dev/stdout"
-        rejected_name = "test_rejects.sam"
+        source_name = "dungeonmap.sam"
+        output_name = "dungeonmap_output.sam"
+        rejected_name = "dungeonmap_rejects.sam"
+        rejected_b_name = "dungeonmap_rejects_b.sam"
+        rejected_p_name = "dungeonmap_rejects_p.sam"
 
     # Open source file in read-only mode
     source = open(source_name, 'r')
-    if source.closed:
-        sys.exit("File open unsuccessful")
 
     # Set up file output
     output = open(output_name, 'w')
     rejected = open(rejected_name, 'w')
+    rejected_b = open(rejected_b_name, 'w')
+    rejected_p = open(rejected_p_name, 'w')
+
+    return source, output, rejected, rejected_b, rejected_p
+
+#-------------------main-----------------------
+
+if __name__ == '__main__':
+
+    # Set up source and output files
+    source, output, rejected, rejected_b, rejected_p = setupio()
 
     # Iterate over lines in sam file passed to program
     for line in source.readlines():
@@ -155,8 +171,13 @@ if __name__ == '__main__':
             output.write(line)
             continue
 
-        elif bwafilter(line, 45, 31):
-            rejected.write(line.rstrip('\n') + " (bwafilter)\n")
+        elif bwa_filter(line, 45, 31):
+            rejected.write(line.rstrip('\n') + " (bwa_filter)\n")
+            rejected_b.write(line)
+
+        elif primer3_filter(line):
+            rejected.write(line.rstrip('\n') + " (primer3_filter)\n")
+            rejected_p.write(line)
 
         else:
             output.write(line)
