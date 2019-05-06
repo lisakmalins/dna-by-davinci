16dfc4b68a319f3e57f1a1f3fb92e62e02132684
Lisa Malins <lkmalins@ucdavis.edu>
2019-05-06 12:49:19 -0700

    Debugging statements removed

--- FilterOligos/FilterOligos.py
+++ FilterOligos/FilterOligos.py
@@ -1,14 +1,13 @@
-# 28 April 2019
+# 6 May 2019
 # Lisa Malins
 # FilterOligos.py
 
 """
 Python program to filter hybridization oligos using bwa and primer3.
 Based on bwa.py and primer3_filter.py from Chorus2 by zhangtaolab on GitHub.
-Currently writes "good" oligos to one file and "bad" oligos to another file for debugging.
 
 Example command:
-python3 FilterOligos.py tinymap.sam test_output.sam test_rejects.sam
+python3 FilterOligos.py unfiltered.sam filtered.sam
 """
 
 import sys
@@ -16,7 +15,8 @@ import re
 try:
     import primer3
 except ImportError:
-    pass
+    print("Primer3 not installed")
+    exit(1)
 
 # Find header lines and just print them
 def isheader(line):
@@ -52,40 +52,21 @@ def bwa_filter(line, min_AS, max_XS):
     # are found, grab the scores and save for comparison to minimum and maximum.
     # If either score isn't found, return true (ignore this line)
 
-
-    mapinfo = line.split('\t') #debug
-
-
     if AS_match:
         AS_score = int(AS_match.group(1))
     else:
-        print("AS score not found for sequence " + mapinfo[0] + " " + mapinfo[14].rstrip('\n')) #debug
         return True
 
     if XS_match:
         XS_score = int(XS_match.group(1))
     else:
-        print("XS score not found for sequence " + mapinfo[0] + " " + mapinfo[14].rstrip('\n')) #debug
         return True
-
-    # print("AS = " + str(AS_score) + " min_AS = " + str(min_AS) + " XS = " + str(XS_score) + " max_XS =" + str(max_XS))
 
 
     # If the alignment score is less than the minimum
     # or the suboptimal alignment score is greater than the maximum,
     # return true (filter out this sequence because we don't like it)
     if (AS_score < min_AS) or (XS_score >= max_XS):
-
-        mapinfo = line.split('\t')
-
-        if (AS_score < min_AS):
-            print(mapinfo[0] + " filtered because AS score " + str(AS_score) + " < " + str(min_AS))
-        if (XS_score >= max_XS):
-            print(mapinfo[0] + " filtered because XS score " + str(XS_score) + " >= " + str(max_XS))
-
-        # print("AS score of " + str(AS_score) + " is greater than or equal to " + str(min_AS))
-        # print("XS score of " + str(XS_score) + " is less than" + str(max_XS))
-
         return True
 
     # Otherwise, if the scores are within range,
@@ -125,8 +106,6 @@ def primer3_filter(line, min_TM=37, max_HTM=35, min_diff_TM=10):
     if (TM - HTM) < min_diff_TM:
         primer3ft = True
 
-    # print(sequence, TM, HTM, dtm)
-
     return primer3ft
 
 # Set up source and output files (returns tuple of file objects)
@@ -135,35 +114,38 @@ def setupio():
     try:
         source_name = sys.argv[1]
         output_name = sys.argv[2]
-        rejected_name = sys.argv[3]
+        # rejected_name = sys.argv[3]
 
     # Use hard-codes file names if arguments not given
     except IndexError:
         source_name = "dungeonmap.sam"
         output_name = "dungeonmap_output.sam"
-        rejected_name = "dungeonmap_rejects.sam"
-        rejected_b_name = "dungeonmap_rejects_b.sam"
-        rejected_p_name = "dungeonmap_rejects_p.sam"
+        # rejected_name = "dungeonmap_rejects.sam"
+        # rejected_b_name = "dungeonmap_rejects_b.sam"
+        # rejected_p_name = "dungeonmap_rejects_p.sam"
 
     # Open source file in read-only mode
     source = open(source_name, 'r')
 
     # Set up file output
     output = open(output_name, 'w')
-    rejected = open(rejected_name, 'w')
-    rejected_b = open(rejected_b_name, 'w')
-    rejected_p = open(rejected_p_name, 'w')
+    # rejected = open(rejected_name, 'w')
+    # rejected_b = open(rejected_b_name, 'w')
+    # rejected_p = open(rejected_p_name, 'w')
 
-    return source, output, rejected, rejected_b, rejected_p
+    # return source, output, rejected, rejected_b, rejected_p
+    return source, output
 
 #-------------------main-----------------------
 
 if __name__ == '__main__':
 
     # Set up source and output files
-    source, output, rejected, rejected_b, rejected_p = setupio()
+    # source, output, rejected, rejected_b, rejected_p = setupio()
+    source, output = setupio()
 
-    # Iterate over lines in sam file passed to program
+
+# Iterate over lines in sam file passed to program
     for line in source.readlines():
 
         # Print headers without touching them
@@ -172,12 +154,14 @@ if __name__ == '__main__':
             continue
 
         elif bwa_filter(line, 45, 31):
-            rejected.write(line.rstrip('\n') + " (bwa_filter)\n")
-            rejected_b.write(line)
+            # rejected.write(line.rstrip('\n') + " (bwa_filter)\n")
+            # rejected_b.write(line)
+            continue
 
-        elif primer3_filter(line):
-            rejected.write(line.rstrip('\n') + " (primer3_filter)\n")
-            rejected_p.write(line)
+        elif primer3_filter(line, 37, 35, 10):
+            # rejected.write(line.rstrip('\n') + " (primer3_filter)\n")
+            # rejected_p.write(line)
+            continue
 
         else:
             output.write(line)
