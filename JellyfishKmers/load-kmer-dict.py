c8f0d460dd0d601d47307d072c3544d9aa78401a
Lisa Malins <lkmalins@ucdavis.edu>
2019-05-13 11:32:32 -0700

    Improved error handling in load-kmer-dict.py. Program outputs informative error messages for accidentally running in script mode, input file not specified, input file not found, and unrecognized input from file.

--- JellyfishKmers/load-kmer-dict.py
+++ JellyfishKmers/load-kmer-dict.py
@@ -1,33 +1,58 @@
-# From python shell:
-# import sys
-# sys.argv = ['filename.fa']
-# exec(open("load-kmer-dict.py").read())
-# sys.getsizeof(scores)
+# 13 May 2019
+# Lisa Malins
+# load-kmer-dict.py
+
+"""
+Reads k-mers and counts from Jellyfish dump file into a dictionary
+in order to measure how much memory is required to hold them.
+
+From python shell:
+sourcename = "{filename.fa}"
+exec(open("load-kmer-dict.py").read())
+"""
+
+import sys
 
 try:
-    sourcename = sys.argv[0]
+    # Make sure script is being run in interactive mode
+    assert sys.argv[0] != "load-kmer-dict.py", "\n" + \
+    "You ran load-kmer-dict.py in script mode.\n" + \
+    "Please open the python shell and rerun in interactive mode " + \
+    "using the following commands:\n" + \
+    "sourcename = \"{filename.fa}\"\n" + \
+    "exec(open(\"load-kmer-dict.py\").read())"
+
+    # Open file
     source = open(sourcename, 'r')
-    print("Reading kmer scores from file " + sys.argv[0] + "...")
+    print("Reading kmer counts from file " + sourcename + "...")
+
+    # Read all k-mers and counts into dictionary
     line = source.readline()
-    scores = dict()
+    counts = dict()
     while line:
-        assert (line[0] == ">")
-        score = line[1:].rstrip('\n')
+        assert line[0] == ">", \
+        "\nUnable to read k-mers and scores due to unexpected input. " + \
+        "Line was:\n" + line.rstrip('\n') + "\nfrom " + sourcename
+        count = line[1:].rstrip('\n')
         line = source.readline()
         seq = line.rstrip('\n')
-        scores[seq] = score
+        counts[seq] = count
         line = source.readline()
-    print(str(len(scores)) + " kmers and scores read from file " + sys.argv[0])
-    print("\tEnter scores to print all entries in dictionary")
-    print("\tEnter sys.getsizeof(scores) to find size of dictionary")
-    print("\tEnter scores['sequence'] to see score for a particular sequence")
+
+    # Output size of dictionary and further commands
+    print(str(len(counts)) + " kmers and counts read from file " + sourcename)
+    print("Current size of dictionary in memory: " + str(sys.getsizeof(counts)))
+    print("Further commands: ")
+    print("\tEnter counts to print all entries in dictionary")
+    print("\tEnter sys.getsizeof(counts) to find size of dictionary")
+    print("\tEnter counts['sequence'] to see count for a particular sequence")
 
 except NameError:
-    print("Please import sys, set filename as sys.argv[0], and try again")
-    print("Make sure you are running this within the python shell or there is no point")
+    print("No jellyfish dump input file specified.")
+    print("Please set filename as follows and try again:\n" + \
+    "sourcename = \"{filename.fa}\"")
 except FileNotFoundError:
-    if sys.argv[0]:
-        print("File " + sys.argv[0] + " not found")
-    print("Please set filename as sys.argv[0] and try again")
-except AssertionError:
-    print("Unexpected input. Line was: " + line.rstrip('\n'))
+    if sourcename:
+        print("File " + sourcename + " not found")
+    print("Please set filename as follows and try again:\n" + \
+    "sourcename = \"{filename.fa}\"")
