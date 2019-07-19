dfc701647f9ddb73e108ab3b654cde90ed9f06d7
Lisa Malins <lkmalins@ucdavis.edu>
2019-07-19 14:53:31 -0700

    Edited setup_bins.sh and binned_read_counts.sh to accept input and output as arguments

--- analysis/binned_read_counts.sh
+++ analysis/binned_read_counts.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# Purpose: Count probes in non-overlapping bins
+# Usage: bash binned_read_counts.sh {}
 
 module load bedtools2/2.27.0 samtools/1.9
-cd /scratch/lmalins/paint/maize-by-michelangelo/data/maps
-BINSIZE=1000000
+SAM=$1
+WINDOWS=$2
+OUTPUT=$3
 
 # convert sam to bam for using bedtools
-samtools view -b -o tinymap.bam tinymap.sam
+samtools view -b $SAM | \
 
 # count reads in non-overlapping bins
-bedtools coverage -a $BINSIZE'_win.bed' -b tinymap.bam > $BINSIZE'_binned_read_counts.bed'
+bedtools coverage -a $WINDOWS -b /dev/fd/0 > $OUTPUT
--- analysis/setup_bins.sh
+++ analysis/setup_bins.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
+# Purpose: Set up windows for binned probe counts
+# Usage: bash setup_bins.sh {map input filename} {windows output filename} {binsize}
+# Default binsize: 1000000 bases
 
 module load bedtools2/2.27.0
-cd /scratch/lmalins/paint/maize-by-michelangelo/data/maps
-BINSIZE=1000000
+MAP=$1
+OUTPUT=$2
+BINSIZE=${3:-1000000}
+
+# Find end of headers
+HEADEREND=`grep -m 1 -v "^@" -n $MAP | cut -f1 -d:`
 
 # make genome file
-grep "^@SQ" tinymap.sam | cut -f 2-3 | sed -e 's/SN://g' -e 's/LN://g' > genome_for_bedtools.txt
+head -n $HEADEREND $MAP | grep "^@SQ" | \
+cut -f 2-3 | sed -e 's/SN://g' -e 's/LN://g' | \
 
 # make bins file
-bedtools makewindows -g genome_for_bedtools.txt -w $BINSIZE > $BINSIZE'_win.bed'
+bedtools makewindows -g /dev/fd/0 -w $BINSIZE > $2
 
 # format of a BED-3 FILE
-# chrome \t start \t end \n
+# chrom \t start \t end \n
 # where start is 0-based coords
