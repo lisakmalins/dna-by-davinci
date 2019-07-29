57db52cf540c45a661cee1b88d596576b18b2f2f
Lisa Malins <lkmalins@ucdavis.edu>
2019-07-29 16:25:55 -0700

    Pasted k-mer and header classes into GetOligos.py

--- GetOligos/GetOligos.py
+++ GetOligos/GetOligos.py
@@ -12,7 +12,6 @@ Output is in fasta format:
     >[chromosome number]_[index]
     [sequence]
 
-K-mer and header classes are in separate files.
 Header class will need to be remade for different assemblies
     since header formats vary (see comments in class)
 Header class lets this file know whether to get k-mers
@@ -28,6 +27,108 @@ python GetOligos.py agra_cadabra_genome.fa 45 3 agra_cadabra_45mers.fa
 import sys
 from ZmaysB73Header import ZmaysHeader
 from Kmer import Kmer
+
+class Kmer:
+
+    # Constructor
+    def __init__(self, f, k, s):
+        self.fo = f         # File to read from
+        self.counter = 0    # Position in file (next letter to read)
+
+        self.k = k          # K-mer size
+        self.s = s          # Step size (how far to advance on next k-mer)
+
+        self.id = 0         # Chromosome ID
+        self.index = 1      # Index within chromosome (refers to starting letter of seq)
+        self.seq = ""       # K-mer sequence
+
+        self.eos = False    # Has program reached the end of the section?
+        self.eof = False    # Has program reached the end of the file?
+
+    # To start new chromosome, reset attributes and get next k-mer
+    def StartNewKMer(self, id):
+        # Set chromosome id
+        self.id = id
+
+        # Set counter as current position of file object
+        self.counter = self.fo.tell()
+
+        # Reset index (position on chromosome) to 1
+        self.index = 1
+
+        # Reset end of section flag
+        self.eos = False
+
+        # Wipe stored k-mer sequence
+        self.seq = ""
+
+        # Read next k letters, starting at stored counter position
+        #   and ignoring non-alphabetical characters
+        while len(self.seq) < self.k:
+            nextChar = self.fo.read(1)
+            if nextChar.isalpha():
+                self.seq += nextChar
+            self.counter += 1
+
+    # To get next k-mer, forget first s letters and append next s letters
+    def GetNextKMer(self):
+        nextLetters = ""
+        self.fo.seek(self.counter, 0)
+
+        # Fetch next s letters, ignoring spaces and newlines
+        while len(nextLetters) < self.s:
+            nextChar = self.fo.read(1)
+            if nextChar:
+                # If next character is a letter, add to nextLetters
+                if nextChar.isalpha():
+                    nextLetters += nextChar
+                    self.index += 1
+                # If next character is >, set eos = true and return to main
+                elif nextChar == ">":
+                    # Backspace file object's counter so header isn't skipped
+                    self.fo.seek(self.counter, 0)
+                    self.eos = True
+                    return
+
+                self.counter += 1
+
+
+            # If next character does not exist, exit function without appending to k-mer
+            else:
+                self.eof = True
+                return
+
+        # Forget first s letters of k-mer and append nextLetters
+        self.seq = self.seq[self.s:] + nextLetters
+
+class ZmaysHeader:
+
+    # Constructor -- accepts header text and extracts info
+    def __init__(self, line):
+        self.line = line                      # entire line
+
+        self.words = line.split()
+        self.type = self.words[1]             # dna:chromosome or dna:contig
+        self.id = self.words[0][1:]           # e.g., chromosome number or Pt, Mt, B73V4_ctg150
+
+        self.words = self.words[2].split(":")
+        self.length = self.words[4]           # length of sequence
+
+    # Evaluate whether we want to read k-mers from a section based on its header
+    # If the header indicates section is a normal, numbered chromosome, returns true
+    # Function needs to be rewritten if different assembly has different header format
+    def EvalHeader(self):
+        if self.type == "dna:chromosome" and self.id.isdigit():
+            return True
+        else:
+            return False
+
+    # Debugging function: Prints all attributes
+    def PrintAll(self):
+        print("Type: ", self.type)
+        print("ID: ", self.id)
+        print("Length: ", self.length)
+        print("Do we care: ", self.EvalHeader(), "\n")
 
 
 # Finds the next header and evaluates whether
