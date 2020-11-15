e9a15ee96a1efafbc16a9255b1830f172e6bf8a0
Lisa Malins <lkmalins@ucdavis.edu>
2020-11-15 13:39:44 -0800

    Minor: whitespace changes only in kmer_count_histogram.R

--- davinci/R/kmer_count_histogram.R
+++ davinci/R/kmer_count_histogram.R
@@ -14,7 +14,10 @@ print(paste("Reading k-mer count histogram from", source))
 print(paste("Saving k-mer count plot to", output))
 
 # Read in data
-kmers <- read_delim(source, delim=" ", col_names = F, col_types='ii')
+kmers <- read_delim(source,
+                    delim = " ",
+                    col_names = F,
+                    col_types = 'ii')
 
 # Find peak
 kmers_abridged <- kmers %>% slice(-1:-10)
@@ -23,9 +26,12 @@ peak <- kmers_abridged[which.max(kmers_abridged$X2), 1] %>% as.numeric()
 # Plot
 ggplot(filter(kmers, X1 >= 4), aes(x = X1, y = X2)) +
   geom_histogram(binwidth = 1, stat = "identity") +
-  scale_x_continuous(limits = c(0, 150), breaks = c(peak, 0, 50, 100, 150)) +
+  scale_x_continuous(limits = c(0, 150),
+                     breaks = c(peak, 0, 50, 100, 150)) +
   scale_y_continuous(limits = c(0, 2e7)) +
-  labs(title = "Number of K-mers per Frequency", x = "Frequency", y = "Number of K-mers") +
+  labs(title = "Number of K-mers per Frequency",
+       x = "Frequency",
+       y = "Number of K-mers") +
   geom_vline(xintercept = peak)
 
 # Save
