2aed3c505bc9657a50d02ee9ce7bf79e12a4308f
Lisa Malins <lkmalins@ucdavis.edu>
2020-11-15 15:09:43 -0800

    Fixed hardcoded x-axis cutoff in davinci/R/kmer_count_histogram.R.
    X-axis cutoff is now set to 0.997 cumulative fraction of number column. In other words, the 0.3% highest abundance k-mers are not plotted.
    
    Should be a more flexible yet effective way to prevent the x-axis being stretched by a teeny tiny number of super-high abundance k-mers.

--- davinci/R/kmer_count_histogram.R
+++ davinci/R/kmer_count_histogram.R
@@ -19,6 +19,13 @@ kmers <- read_delim(source,
                     col_names = c("abundance", "number"),
                     col_types = 'ii')
 
+# Add columns for cumulative sum and cumulative percent
+kmers <- kmers %>%
+  # Cumulative sum of number column
+  mutate(cum_sum = cumsum(number)) %>%
+  # Cumulative fraction of number column
+  mutate(cum_fraction = cum_sum / sum(number))
+
 # Find peak
 # Ignore left tail
 kmers_abridged <- kmers %>% slice(-1:-10)
@@ -29,14 +36,18 @@ peak_x <- kmers_abridged[[which.max(kmers_abridged$number), "abundance"]]
 print(paste("Peak detected at abundance", peak_x,
             "with", peak_y, "k-mers"))
 
+# X-axis cutoff at 0.997 cumulative fraction
+# Fixes x-axis stretched by tiny number of high-abundance k-mers
+x_cutoff <- min(which(kmers$cum_fraction > 0.997))
 # Y-axis cutoff will be 110% of y_peak
 y_cutoff <- round(peak_y * 1.1)
 
 # Plot
 ggplot(filter(kmers, abundance >= 4), aes(x = abundance, y = number)) +
   geom_histogram(binwidth = 1, stat = "identity") +
-  scale_x_continuous(limits = c(0, 150),
-                     breaks = c(peak_x, 0, 50, 100, 150)) +
+  scale_x_continuous(limits = c(0, x_cutoff),
+                     breaks = c(seq(from=0, to=x_cutoff, by=10),
+                                peak_x)) +
   scale_y_continuous(limits = c(0, y_cutoff)) +
   geom_vline(xintercept = peak_x) +
   labs(title = "Number of K-mers by Abundance",
