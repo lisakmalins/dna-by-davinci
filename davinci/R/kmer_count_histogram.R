587fe09d82a5db44faf60993489ddc60737cd099
Lisa Malins <lkmalins@ucdavis.edu>
2020-11-15 13:45:16 -0800

    Minor: added colnames and tweaked plot labels in kmer_count_histogram.R

--- davinci/R/kmer_count_histogram.R
+++ davinci/R/kmer_count_histogram.R
@@ -16,22 +16,22 @@ print(paste("Saving k-mer count plot to", output))
 # Read in data
 kmers <- read_delim(source,
                     delim = " ",
-                    col_names = F,
+                    col_names = c("abundance", "number"),
                     col_types = 'ii')
 
 # Find peak
 kmers_abridged <- kmers %>% slice(-1:-10)
-peak <- kmers_abridged[which.max(kmers_abridged$X2), 1] %>% as.numeric()
+peak <- kmers_abridged[which.max(kmers_abridged$number), 1] %>% as.numeric()
 
 # Plot
-ggplot(filter(kmers, X1 >= 4), aes(x = X1, y = X2)) +
+ggplot(filter(kmers, abundance >= 4), aes(x = abundance, y = number)) +
   geom_histogram(binwidth = 1, stat = "identity") +
   scale_x_continuous(limits = c(0, 150),
                      breaks = c(peak, 0, 50, 100, 150)) +
   scale_y_continuous(limits = c(0, 2e7)) +
-  labs(title = "Number of K-mers per Frequency",
-       x = "Frequency",
-       y = "Number of K-mers") +
+  labs(title = "Number of K-mers by Abundance",
+       x = "K-mer abundance in reads",
+       y = "Number of distinct k-mers") +
   geom_vline(xintercept = peak)
 
 # Save
