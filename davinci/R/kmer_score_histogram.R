b77549c699512fc6e25dbe47781a068a79873e79
Lisa Malins <lkmalins@ucdavis.edu>
2020-12-29 23:07:55 -0800

    kmer_score_histogram.R now accepts a list of vertical lines to draw on histogram.
    Planning to use this feature to draw the predicted score peak and filtering limits on the histogram.
    
    Argument must be comma-separated list of integers, or left blank. (Script dies loudly if argument contains illegal characters.)
    
    I think my implementation is pretty clever. I avoided if statements in the ggplot code, because I discovered that passing an empty vector to geom_vline() and annotate() makes them draw nothing.

--- davinci/R/kmer_score_histogram.R
+++ davinci/R/kmer_score_histogram.R
@@ -13,6 +13,25 @@ output = args[2]
 print(paste("Reading k-mer score histogram from", source))
 print(paste("Saving k-mer score plot to", output))
 
+# Optional: accept comma-separated-list of vertical lines to draw on histogram
+if (length(args) >= 3) {
+  # Verify argument contains no illegal characters
+  if (!grepl("^[0-9,]*$", args[3])) {
+    stop(paste("Optional third argument to this script accepts\n",
+               "a list of vertical lines to draw on the histogram.\n",
+               "Must be a comma-separated list of integers.\n",
+               "For example: 100,200,300\n",
+               "Unexpected input was:", vlines_argument))
+  }
+
+  # Split argument into vector of integers
+  vlines <- args[3] %>% strsplit(",") %>% unlist() %>% as.integer()
+
+} else {
+  # If no argument provided, save empty vector instead
+  vlines <- vector(mode = "integer")
+}
+
 # Read in data
 score_histo <- read_csv(source,
                         col_types="ii")
@@ -26,7 +45,14 @@ ggplot(score_histo, aes(x = score, y = frequency)) +
   # Zoom x-axis without clipping
   coord_cartesian(xlim=c(0, x_cutoff)) +
   # Titles
-  labs(title = "K-mer Score Distribution", x = "K-mer score", y = "Frequency")
+  labs(title = "K-mer Score Distribution", x = "K-mer score", y = "Frequency") +
+  # Draw vertical lines on histogram if provided
+  # Does nothing if vlines is empty vector
+  geom_vline(xintercept=vlines, color="#181818") +
+  annotate(geom = "label",
+           label = vlines,
+           x = vlines,
+           y = max(score_histo$frequency)*1.05)
 
 # Save
 ggsave(output, plot = last_plot())
