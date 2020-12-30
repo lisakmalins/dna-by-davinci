1f50141b7d2f39543b3a6557aa296c8934d24010
Lisa Malins <lkmalins@ucdavis.edu>
2020-12-29 22:52:03 -0800

    Minor: added comments to kmer_score_histogram.R

--- davinci/R/kmer_score_histogram.R
+++ davinci/R/kmer_score_histogram.R
@@ -17,11 +17,15 @@ print(paste("Saving k-mer score plot to", output))
 score_histo <- read_csv(source,
                         col_types="ii")
 
+x_cutoff = 3000
+
 # Plot
 ggplot(score_histo, aes(x = score, y = frequency)) +
+  # Histogram has already been computed so use geom_col() to display
   geom_col() +
   # Zoom x-axis without clipping
-  coord_cartesian(xlim=c(0, 3000)) +
+  coord_cartesian(xlim=c(0, x_cutoff)) +
+  # Titles
   labs(title = "K-mer Score Distribution", x = "K-mer score", y = "Frequency")
 
 # Save
