87385370bd808b53ea5f7dd69b7fe9d52a568cae
Lisa Malins <lkmalins@ucdavis.edu>
2021-01-01 20:56:17 -0800

    In kmer_score_histogram.R, added print statements to echo vertical line argument.

--- davinci/R/kmer_score_histogram.R
+++ davinci/R/kmer_score_histogram.R
@@ -24,6 +24,9 @@ if (length(args) >= 3) {
                "Must be a comma-separated list of integers.\n",
                "For example: 100,200,300\n",
                "Unexpected input was:", vlines_argument))
+  } else {
+    print("Vertical line argument detected.")
+    print(paste("Vertical lines will be drawn at the following x-coordinates:", args[3]))
   }
 
   # Split argument into vector of integers
